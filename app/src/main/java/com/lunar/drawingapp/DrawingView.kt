65c9c96e5ba6e28e39a084a8ef1b2a92e2084049
Roshadgu <roshadgu@gmail.com>
2021-02-03 21:50:14 -0500

    Created brush buttons but app will not load

--- app/src/main/java/com/lunar/drawingapp/DrawingView.kt
+++ app/src/main/java/com/lunar/drawingapp/DrawingView.kt
@@ -3,6 +3,7 @@ package com.lunar.drawingapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 import java.security.AccessControlContext
@@ -33,7 +34,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs)
         mDrawPaint!!.strokeJoin = Paint.Join.ROUND
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.toFloat()
+        //mBrushSize = 20.toFloat()
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int)
@@ -102,8 +103,12 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs)
         return true
     }
 
-    internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path()
+    fun setSizeForBrush(newSize: Float)
     {
-
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, newSize, resources.displayMetrics)
+        mDrawPaint!!.strokeWidth = mBrushSize
     }
+
+    //Inner class for custom path with two params as color and stroke size
+    internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path()
 }
--- app/src/main/java/com/lunar/drawingapp/MainActivity.kt
+++ app/src/main/java/com/lunar/drawingapp/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.lunar.drawingapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
+  override fun onCreate(savedInstanceState: Bundle?)
+  {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    val drawing_view = findViewById<DrawingView>(R.id.drawing_view)
+    drawing_view.setSizeForBrush(20.toFloat())
+
+    val brush = findViewById<ImageButton>(R.id.brush)
+    brush.setOnClickListener{
+      showBrushSizeChooserDialog()
+    }
+  }
+
+  private fun showBrushSizeChooserDialog()
+  {
+    val drawing_view = findViewById<DrawingView>(R.id.drawing_view)
+    val brushDialog = Dialog(this)
+    brushDialog.setContentView(R.layout.dialog_brush_size)
+    brushDialog.setTitle("Brush Size: ")
+    val smallBtn = brushDialog.findViewById<ImageButton>(R.id.small_brush)
+    smallBtn.setOnClickListener{
+      drawing_view.setSizeForBrush(10.toFloat())
+      brushDialog.dismiss()
+    }
+
+    val mediumBtn = brushDialog.findViewById<ImageButton>(R.id.medium_brush)
+    mediumBtn.setOnClickListener{
+      drawing_view.setSizeForBrush(20.toFloat())
+      brushDialog.dismiss()
+    }
+
+    val largeBtn = brushDialog.findViewById<ImageButton>(R.id.large_brush)
+    largeBtn.setOnClickListener{
+      drawing_view.setSizeForBrush(30.toFloat())
+      brushDialog.dismiss()
+    }
+    brushDialog.show()
   }
 }
